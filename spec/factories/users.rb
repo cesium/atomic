62add53a3845a2599b19ad4ad140bc6e30368906
Fernando Mendes <devfrmendes@gmail.com>
2015-09-04 20:18:49 +0100

    Adds User, Member and Buddy factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,20 @@
 FactoryGirl.define do
   factory :user do
-    name "MyString"
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+
+    name "#{first_name} #{last_name}"
+    sequence(:account_number)
+    sequence(:student_id)
+    phone_number Faker::Number.number(9)
+    street Faker::Address.street_address
+    city Faker::Address.city
+    birthdate Faker::Date.between(30.years.ago, 18.years.ago)
   end
 
+  factory :member, parent: :user do
+  end
+
+  factory :buddy, parent: :user do
+  end
 end
