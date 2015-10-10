6e1a92ce65a5273ed72ad2e1b1f704516d8e8ae8
JoaoFCosta <dinojoaocosta@gmail.com>
2015-10-10 19:01:47 +0100

    Remove year from Term model

--- db/migrate/20151010143258_create_terms.rb
+++ db/migrate/20151010143258_create_terms.rb
@@ -1,7 +1,6 @@
 class CreateTerms < ActiveRecord::Migration
   def change
     create_table :terms do |t|
-      t.integer     :year
       t.references  :user,  foreign_key: true
       t.references  :role,  foreign_key: true
       t.references  :board, foreign_key: true
