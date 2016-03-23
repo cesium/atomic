07028557b96afb29283bfd6aad85e1a231d0c53a
JoaoFCosta <dinojoaocosta@gmail.com>
2016-03-23 22:19:38 +0000

    Remove materialize from views.
    - Remove materialize classes from boards, roles, departments and terms
      views.
    - Update term new.html.erb form to use the role and department names
      instead of ids.

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,15 +1,17 @@
 class TermsController < ApplicationController
   def new
+    @roles        = Role.all.pluck(:title)
+    @departments  = Department.all.pluck(:title)
   end
 
   def create
     @term = Term.new(params.require(:term).permit(:user_id, :role_id, :board_id, :department_id))
     @term.save
 
-    User.find(params[:term][:user_id]).terms              << @term
-    Role.find(params[:term][:role_id]).terms              << @term
-    Board.find(params[:board_id]).terms                   << @term
-    Department.find(params[:term][:department_id]).terms  << @term
+    User.find(params[:term][:user_id]).terms                            << @term
+    Role.where(title: params[:term][:role_id]).first.terms              << @term
+    Board.find(params[:board_id]).terms                                 << @term
+    Department.where(title: params[:term][:department_id]).first.terms  << @term
 
     redirect_to board_path(params[:board_id])
   end
