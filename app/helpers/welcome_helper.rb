047bf1f87974b9a2b5805bba27738582cae2a854
Martinho Aragão <martinhoaragao@gmail.com>
2016-08-06 18:38:50 +0100

    Update home carousel

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,11 @@
 module WelcomeHelper
+  def carousel_image_tag(file, data = {}, **args)
+    default_data = {
+      bgfit: 'cover',
+      bgposition: 'center',
+      bgrepeat: 'no-repeat'
+    }.merge(data)
+    (args[:data] && args[:data].reverse_merge!(default_data)) || args[:data] = default_data
+    image_tag(file, args)
+  end
 end
