563c16f27fdaf8a8965b0ddf7a114d12f11cd4c1
Martinho Aragão <martinhoaragao@gmail.com>
2016-09-12 15:25:35 +0100

    Update gemfile and move deploy to use ma-deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:cesium/atomic.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :branch, "ma-deploy"
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/usr/share/nginx/atomic'
