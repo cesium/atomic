48b0be34c0b13479ee7b20ffede442e0c7a5e0ab
Martinho Aragão <martinhoaragao@gmail.com>
2017-04-17 11:37:39 +0100

    Move .env to capistrano's shared and update .env.example

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :branch, ENV['BRANCH'] if ENV['BRANCH']
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{.env config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
