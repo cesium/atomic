3a4dff526a322337f6febdaaa3b502ec06ae2345
escolhido <celocaridade@gmail.com>
2017-10-06 22:10:29 +0100

    Changes authentication mecanisms to cancancan

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.activity_admin?
-      can :manage, [Activity, Registration]
-    else
+
+    case user.permissions
+    when :guest
+      can :read, Activity
+    when :user
       can :read, Activity
       can [:create, :destroy], Registration
+    when :activity_admin
+      can :manage, [Activity, Registration]
     end
   end
-
 end
