ef5646d347f448be6ca8479e2804ea2b2a09f106
escolhido <marcelo.caridade@protonmail.com>
2017-10-24 19:17:38 +0100

    Translate date and change format

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module Atomic
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.i18n.default_locale = :pt
   end
 end
