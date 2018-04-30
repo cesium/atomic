d01f93022b25f2e6c382b8925f0437937456ce87
0urobor0s <lhome.gamer@gmail.com>
2018-04-30 21:59:02 +0100

    Fix factories for tests

--- spec/factories/partners.rb
+++ spec/factories/partners.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :partner do
     name { Faker::BossaNova.song }
     description { Faker::Hacker.say_something_smart }
-    logo { Faker::Avatar.image("cesium", "300x300", "jpg") }
   end
 end
