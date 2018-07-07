d2dd295a5b0923ef5d7c5532e1ba2c14c0b011ed
João Vilaça <machadovilaca@gmail.com>
2018-07-08 00:43:17 +0100

    fix views if fields are empty, remove duplicate code

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -8,26 +8,4 @@ module JobsHelper
   def error_messages(job)
     job.errors.full_messages
   end
-
-  def to_markdown(text)
-    Redcarpet::Markdown
-      .new(Redcarpet::Render::HTML, markdown_config)
-      .render(text)
-      .html_safe
-  end
-
-  def markdown_config
-    {
-      no_intra_emphasis: true,
-      fenced_code_blocks: true,
-      disable_indented_code_blocks: true,
-      autolink: true
-    }
-  end
-
-  def to_plaintext(markdown)
-    Redcarpet::Markdown
-      .new(Redcarpet::Render::StripDown)
-      .render(markdown)
-  end
 end
