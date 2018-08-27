e6c0661309bf83343e1a8df589b9090b2a717a6f
João Vilaça <machadovilaca@gmail.com>
2018-08-27 19:47:23 +0100

    Fix style

--- app/models/job.rb
+++ app/models/job.rb
@@ -15,11 +15,11 @@ class Job < ApplicationRecord
 
   def all_tags=(names)
     self.tags = names.split(",").map do |name|
-        Tag.where(name: name.strip).first_or_create!
+      Tag.where(name: name.strip).first_or_create!
     end
   end
 
   def all_tags
-    self.tags.map(&:name).join(", ")
+    tags.map(&:name).join(", ")
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-    has_many :job_taggings
-    has_many :jobs, through: :job_taggings
+  has_many :job_taggings
+  has_many :jobs, through: :job_taggings
 end
