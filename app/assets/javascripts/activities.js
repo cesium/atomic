726ea38fc077ef35db2cd1e22fec96a0f11b162c
ZePedroResende <j.p.resende68@gmail.com>
2018-10-20 22:11:47 +0100

    Fix update and edit forms

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-  var checkbox = document.getElementById("activity_limit_number_participants");
-  var detailsDiv = document.getElementById("is-number-participants-limited");
+  toggle_registrations("activity_allows_registrations", "allows_registration");
+  toggle_registrations("activity_limit_number_participants", "is-number-participants-limited");
+});
+
+function toggle_registrations(checkbox, detailsDiv){
+  var checkbox = document.getElementById(checkbox);
+  var detailsDiv = document.getElementById(detailsDiv);
+
+  detailsDiv.hidden = !checkbox.checked;
   checkbox.onchange = function() {
     detailsDiv.hidden = !this.checked;
   };
-});
+}
