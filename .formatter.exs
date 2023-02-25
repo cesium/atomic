2ec069ce3725768b051bb0efcb4d715fa0f53602
feliciofilipe <filipe.felicio.1999@gmail.com>
2023-02-25 01:42:13 +0000

    Configure formatter for HTML

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,12 @@
 [
   import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  heex_line_length: 300,
+  inputs: [
+    "*.{heex,ex,exs}",
+    "priv/*/seeds.exs",
+    "priv/repo/seeds/*.exs",
+    "{config,lib,test}/**/*.{heex,ex,exs}"
+  ],
   subdirectories: ["priv/*/migrations"]
 ]
