77e56a6e7898c578704153cd51f61f6d26193f5b
Rui Oliveira <rpoliveira02@gmail.com>
2023-03-17 14:58:13 +0000

    Allow students to sign up to organizations

--- lib/atomic_web/live/organization_live/show.ex
+++ lib/atomic_web/live/organization_live/show.ex
@@ -16,6 +16,26 @@ defmodule AtomicWeb.OrganizationLive.Show do
      |> assign(:organization, Organizations.get_organization!(id, [:departments]))}
   end
 
+  @impl true
+  def handle_event("sign_up", _params, socket) do
+    attrs = %{
+      accepted: false,
+      user_id: socket.assigns.current_user.id,
+      organization_id: socket.assigns.organization.id
+    }
+
+    case Organizations.create_association(attrs) do
+      {:ok, _organization} ->
+        {:noreply,
+         socket
+         |> put_flash(:success, "Association created successfully")
+         |> push_redirect(to: Routes.organization_index_path(socket, :index))}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
   defp page_title(:show), do: "Show Organization"
   defp page_title(:edit), do: "Edit Organization"
 end
