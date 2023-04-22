24a63b742f46e06e10239311233d0a324b63471b
Rui Oliveira <rpoliveira02@gmail.com>
2023-04-22 22:40:22 +0100

    CI

--- lib/atomic/quantum/certificate_delivery.ex
+++ lib/atomic/quantum/certificate_delivery.ex
@@ -54,14 +54,10 @@ defmodule Atomic.Quantum.CertificateDelivery do
     end
   end
 
-  @doc """
+  # Generates the PDF file for the given enrollment.
 
-  Generates the PDF file for the given enrollment.
-
-  It uses `wkhtmltopdf` to build it from an HTML template, which
-  is rendered beforehand.
-
-  """
+  # It uses `wkhtmltopdf` to build it from an HTML template, which
+  # is rendered beforehand.
   defp generate_certificate(%Enrollment{} = enrollment) do
     # Create the string corresponding to the HTML to convert
     # to a PDF
@@ -87,19 +83,15 @@ defmodule Atomic.Quantum.CertificateDelivery do
     )
   end
 
-  @doc """
+  # Builds the query to determine the activities to consider for certificate
+  # delivery.
 
-  Builds the query to determine the activities to consider for certificate
-  delivery.
+  # An activity is considered if its last session
+  # ended less than 24h ago. The last session is the one which finishes
+  # later (not necessary the one that starts later).
 
-  An activity is considered if its last session
-  ended less than 24h ago. The last session is the one which finishes
-  later (not necessary the one that starts later).
-
-  The result of the query is the IDs of the activities, not the structs
-  themselves.
-
-  """
+  # The result of the query is the IDs of the activities, not the structs
+  # themselves.
   defp last_sessions_query() do
     now = DateTime.utc_now()
     minimum_finish = DateTime.add(now, -24, :hour)
@@ -114,19 +106,16 @@ defmodule Atomic.Quantum.CertificateDelivery do
       select: s.activity_id
   end
 
-  @doc """
+  # Determines all the enrollments eligible to receive participation
+  # certificates.
 
-  Determines all the enrollments eligible to receive participation
-  certificates.
+  # An enrollment is eligible if and only if:
 
-  An enrollment is eligible if and only if:
+  # - The activity it refers to has ended less than 24h ago (meaning its last
+  # session ended less than 24h ago)
+  # - The user has participated in the session (meaning the present field
+  # of the enrollment will be true)
 
-  - The activity it refers to has ended less than 24h ago (meaning its last
-  session ended less than 24h ago)
-  - The user has participated in the session (meaning the present field
-  of the enrollment will be true)
-
-  """
   defp included_enrollments() do
     enrollments =
       from s in subquery(last_sessions_query()),
