75bafaeec5006613db3ed6a8f14fb37e36d821b6
João Lobo <joaodlobo@gmail.com>
2023-07-05 15:59:30 +0100

    Initial role based routing

---
+++ lib/atomic_web/allowed_roles.ex
@@ -0,0 +1,18 @@
+defmodule AtomicWeb.Auth.AllowedRoles do
+  @moduledoc false
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, opts) do
+
+    if conn.assigns.current_user.role in opts do
+      conn
+    else
+      conn
+      |> send_resp(:not_found, "")
+      |> halt()
+    end
+  end
+
+end
