88fec9ba2538676c1397a5677e3bd56dd46b9950
Júlio Pinto <pinto.julioj03@gmail.com>
2023-07-12 03:00:27 +0100

    fix: add styling to heex code

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -10,13 +10,23 @@ module.exports = {
     '../lib/*_web/**/*.*ex',
   ],
   theme: {
-    extend: {},
+    colors: {
+      'cesium-900': '#ed7950',
+      'cesium-800': '#ed8661',
+      'cesium-700': '#f1a184',
+      'cesium-600': '#F3AE95',
+      'cesium-500': '#F5BCA7',
+      'cesium-400': '#F7C9B9',
+      'cesium-300': '#F9D7CA',
+      'cesium-200': '#FBE4DC',
+      'cesium-100': '#FFEDE7'
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
-    plugin(({addVariant}) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
-    plugin(({addVariant}) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
-    plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
+    plugin(({ addVariant }) => addVariant('phx-no-feedback', ['&.phx-no-feedback', '.phx-no-feedback &'])),
+    plugin(({ addVariant }) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
   ]
 }
