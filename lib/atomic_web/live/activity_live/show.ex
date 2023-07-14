1c1187a94e94dd5849c76784bfaf8cba402455ce
João Lobo <joaodlobo@gmail.com>
2023-07-15 00:22:13 +0100

    Solve warnings

--- lib/atomic_web/live/activity_live/show.ex
+++ lib/atomic_web/live/activity_live/show.ex
@@ -77,7 +77,7 @@ defmodule AtomicWeb.ActivityLive.Show do
   def handle_event("delete", _payload, socket) do
     {:ok, _} = Activities.delete_activity(socket.assigns.activity)
 
-    {:noreply, push_redirect(socket, to: Routes.activity_index_path(socket, :index))}
+    {:noreply, push_redirect(socket, to: Routes.activity_index_path(socket, :index, socket.assigns.current_organization))}
   end
 
   @impl true
