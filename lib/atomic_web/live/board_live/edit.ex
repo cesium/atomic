aaf5d8c15abf262de94bd69f470e968f975527f0
João Lobo <joaodlobo@gmail.com>
2023-07-14 23:36:39 +0100

    Change board routes and check for id mismatches

--- lib/atomic_web/live/board_live/edit.ex
+++ lib/atomic_web/live/board_live/edit.ex
@@ -10,16 +10,20 @@ defmodule AtomicWeb.BoardLive.Edit do
   end
 
   @impl true
-  def handle_params(%{"org" => _org, "id" => id}, _url, socket) do
+  def handle_params(%{"organization_id" => organization_id, "id" => id}, _url, socket) do
     user_organization = Organizations.get_user_organization!(id, [:user, :organization])
     users = Enum.map(Accounts.list_users(), fn u -> [key: u.email, value: u.id] end)
 
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:user_organization, user_organization)
-     |> assign(:users, users)
-     |> assign(:current_user, socket.assigns.current_user)}
+    if user_organization.organization_id == organization_id do
+      {:noreply,
+        socket
+        |> assign(:page_title, page_title(socket.assigns.live_action))
+        |> assign(:user_organization, user_organization)
+        |> assign(:users, users)
+        |> assign(:current_user, socket.assigns.current_user)}
+    else
+      raise AtomicWeb.MismatchError
+    end
   end
 
   defp page_title(:index), do: "List board"
