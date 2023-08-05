ef44a9b496b7106d7171704bcce41d647aadbe29
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-08-06 00:06:14 +0100

    Change sidebar based on role

--- lib/atomic_web/config.ex
+++ lib/atomic_web/config.ex
@@ -2,9 +2,19 @@ defmodule AtomicWeb.Config do
   @moduledoc """
   Web configuration for our pages.
   """
+  alias Atomic.Organizations
   alias AtomicWeb.Router.Helpers, as: Routes
 
-  def pages(conn, current_organization) do
+  def pages(conn, current_organization, current_user) do
+    if current_user.role in [:admin] or
+         Organizations.get_role(current_user.id, current_organization.id) in [:admin, :owner] do
+      admin_pages(conn, current_organization)
+    else
+      user_pages(conn, current_organization)
+    end
+  end
+
+  def admin_pages(conn, current_organization) do
     [
       %{
         key: :home,
@@ -67,6 +77,67 @@ defmodule AtomicWeb.Config do
         title: "Scanner",
         icon: :qrcode,
         url: Routes.scanner_index_path(conn, :index),
+        tabs: []
+      },
+      %{
+        key: :profile,
+        title: "Settings",
+        icon: :cog,
+        url: Routes.user_settings_path(conn, :edit),
+        tabs: []
+      }
+    ]
+  end
+
+  def user_pages(conn, current_organization) do
+    [
+      %{
+        key: :home,
+        title: "Home",
+        icon: :home,
+        url: Routes.home_index_path(conn, :index),
+        tabs: []
+      },
+      %{
+        key: :calendar,
+        title: "Calendar",
+        icon: :calendar,
+        url: Routes.calendar_show_path(conn, :show),
+        tabs: []
+      },
+      %{
+        key: :activities,
+        title: "Activities",
+        icon: :academic_cap,
+        url: Routes.activity_index_path(conn, :index, current_organization.id),
+        tabs: []
+      },
+      %{
+        key: :departments,
+        title: "Departments",
+        icon: :cube,
+        url: Routes.department_index_path(conn, :index, current_organization.id),
+        tabs: []
+      },
+      %{
+        key: :partners,
+        title: "Partners",
+        icon: :user_group,
+        url: Routes.partner_index_path(conn, :index, current_organization.id),
+        tabs: []
+      },
+      %{
+        key: :board,
+        title: "Board",
+        icon: :users,
+        url: Routes.board_index_path(conn, :index, current_organization.id),
+        tabs: []
+      },
+      %{
+        key: :organizations,
+        title: "Organizations",
+        icon: :office_building,
+        url: Routes.organization_index_path(conn, :index),
         tabs: []
       },
       %{
