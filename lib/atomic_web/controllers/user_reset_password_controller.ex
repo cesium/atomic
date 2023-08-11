4e4b7c3fdfc7f626387b62ef61201840ad238f02
Rui Oliveira <rpoliveira02@gmail.com>
2023-08-11 21:06:30 +0100

    Reset password page

--- lib/atomic_web/controllers/user_reset_password_controller.ex
+++ lib/atomic_web/controllers/user_reset_password_controller.ex
@@ -6,7 +6,7 @@ defmodule AtomicWeb.UserResetPasswordController do
   plug :get_user_by_reset_password_token when action in [:edit, :update]
 
   def new(conn, _params) do
-    render(conn, "new.html", error_message: nil, submit: false)
+    render(conn, "new.html", error_message: nil)
   end
 
   def create(conn, %{"user" => %{"email" => email}}) do
@@ -26,7 +26,7 @@ defmodule AtomicWeb.UserResetPasswordController do
   end
 
   def edit(conn, _params) do
-    render(conn, "edit.html", changeset: Accounts.change_user_password(conn.assigns.user))
+    render(conn, "edit.html", changeset: Accounts.change_user_password(conn.assigns.user), error_message: nil)
   end
 
   # Do not log in the user after reset password to avoid a
@@ -51,8 +51,7 @@ defmodule AtomicWeb.UserResetPasswordController do
     else
       conn
       |> put_flash(:error, "Reset password link is invalid or it has expired.")
-      |> redirect(to: "/")
-      |> halt()
+      |> redirect(to: "/404")
     end
   end
 end
