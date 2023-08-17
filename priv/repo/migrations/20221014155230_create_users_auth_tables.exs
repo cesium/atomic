f952cf056bdea33f5a3e942f234c8f2dbc7db4d0
João Lobo <joaodlobo@gmail.com>
2023-08-17 15:31:19 +0100

    Add unique constraint to handle column

--- priv/repo/migrations/20221014155230_create_users_auth_tables.exs
+++ priv/repo/migrations/20221014155230_create_users_auth_tables.exs
@@ -21,7 +21,7 @@ defmodule Atomic.Repo.Migrations.CreateUsersAuthTables do
       timestamps()
     end
 
-    create unique_index(:users, [:email])
+    create unique_index(:users, [:email, :handle])
 
     create table(:users_tokens, primary_key: false) do
       add :id, :binary_id, primary_key: true
