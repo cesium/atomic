fb4bbb2e353cd6e0e35316d2fd8ee432b6abd765
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-08-18 22:20:18 +0100

    build: bump Elixir version (#325)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Atomic.MixProject do
     [
       app: :atomic,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
