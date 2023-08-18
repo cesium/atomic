38e1a06d68685f33c52f56ac688eb386db263575
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-08-18 23:55:14 +0100

    fix: seeds (#312)

--- priv/repo/seeds/memberships.exs
+++ priv/repo/seeds/memberships.exs
@@ -2,7 +2,6 @@ defmodule Atomic.Repo.Seeds.Memberships do
   alias Atomic.Organizations.Organization
   alias Atomic.Organizations.Membership
   alias Atomic.Accounts.User
-  alias Atomic.Organizations
   alias Atomic.Organizations.Board
   alias Atomic.Organizations.BoardDepartments
   alias Atomic.Organizations.UserOrganization
@@ -16,74 +15,107 @@ defmodule Atomic.Repo.Seeds.Memberships do
   end
 
   def seed_memberships() do
-    users = Repo.all(User)
-    organizations = Repo.all(Organization)
+    case Repo.all(Membership) do
+      [] ->
+        users = Repo.all(User)
+        organizations = Repo.all(Organization)
 
-    for user <- users do
-      for organization <- organizations do
-        prob = 50
-        random_number = :rand.uniform(100)
+        for user <- users do
+          for organization <- organizations do
+            prob = 50
+            random_number = :rand.uniform(100)
 
-        if random_number < prob do
-          %Membership{}
-          |> Membership.changeset(%{
-            "user_id" => user.id,
-            "organization_id" => organization.id,
-            "created_by_id" => Enum.random(users).id,
-            "role" => Enum.random([:follower, :member, :admin, :owner])
-          })
-          |> Repo.insert!()
+            if random_number < prob do
+              %Membership{}
+              |> Membership.changeset(%{
+                "user_id" => user.id,
+                "organization_id" => organization.id,
+                "created_by_id" => Enum.random(users).id,
+                "role" => Enum.random([:follower, :member, :admin, :owner])
+              })
+              |> Repo.insert!()
+            end
+          end
         end
-      end
+
+      _ ->
+        Mix.shell().error("Found memberships, aborting seeding memberships.")
     end
   end
 
   def seed_board do
-    organizations = Repo.all(Organization)
+    case Repo.all(Board) do
+      [] ->
+        organizations = Repo.all(Organization)
 
-    for organization <- organizations do
-      %Board{}
-      |> Board.changeset(%{
-        "organization_id" => organization.id,
-        "year" => "2023/2024"
-      })
-      |> Repo.insert!()
+        for organization <- organizations do
+          %Board{}
+          |> Board.changeset(%{
+            "organization_id" => organization.id,
+            "year" => "2023/2024"
+          })
+          |> Repo.insert!()
+        end
+
+      _ ->
+        Mix.shell().error("Found boards, aborting seeding boards.")
     end
   end
 
   def seed_board_departments do
-    departments = ["Conselho Fiscal", "Mesa AG", "CAOS", "DMC", "DMP", "Presidência", "Vogais"]
-    boards = Repo.all(Board)
+    case Repo.all(BoardDepartments) do
+      [] ->
+        departments = [
+          "Conselho Fiscal",
+          "Mesa AG",
+          "CAOS",
+          "DMC",
+          "DMP",
+          "Presidência",
+          "Vogais"
+        ]
 
-    for board <- boards do
-      for i <- 0..6 do
-        %BoardDepartments{}
-        |> BoardDepartments.changeset(%{
-          "board_id" => board.id,
-          "name" => Enum.at(departments, i),
-          "priority" => i
-        })
-        |> Repo.insert!()
-      end
+        boards = Repo.all(Board)
+
+        for board <- boards do
+          for i <- 0..6 do
+            %BoardDepartments{}
+            |> BoardDepartments.changeset(%{
+              "board_id" => board.id,
+              "name" => Enum.at(departments, i),
+              "priority" => i
+            })
+            |> Repo.insert!()
+          end
+        end
+
+      _ ->
+        Mix.shell().error("Found board departments, aborting seeding board departments.")
     end
   end
 
   def seed_user_organizations do
-    titles = ["Presidente", "Vice-Presidente", "Secretário", "Tesoureiro", "Vogal"]
-    board_departments = Repo.all(BoardDepartments)
-    users = Repo.all(User)
+    case Repo.all(UserOrganization) do
+      [] ->
+        titles = ["Presidente", "Vice-Presidente", "Secretário", "Tesoureiro", "Vogal"]
+        users = Repo.all(User)
+        board_departments = Repo.all(BoardDepartments)
 
-    for board_department <- board_departments do
-      for i <- 0..4 do
-        %UserOrganization{}
-        |> UserOrganization.changeset(%{
-          "user_id" => Enum.random(users).id,
-          "board_departments_id" => board_department.id,
-          "role" => Enum.at(titles, i),
-          "priority" => i
-        })
-        |> Repo.insert!()
-      end
+        for board_department <- board_departments do
+          for i <- 0..4 do
+            %UserOrganization{}
+            |> UserOrganization.changeset(%{
+              "user_id" => Enum.random(users).id,
+              "board_departments_id" => board_department.id,
+              "role" => Enum.at(titles, i),
+              "priority" => i
+            })
+            |> Repo.insert!()
+          end
+        end
+
+      _ ->
+        Mix.shell().error("Found user organizations, aborting seeding user organizations.")
     end
   end
 end
