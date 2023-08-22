689c8b1e244bb267cdf7482d573926901f52bf05
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-08-23 00:20:24 +0100

    Update scripts/git.sh
    Co-authored-by: Mário Rodrigues <93675410+MarioRodrigues10@users.noreply.github.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -8,14 +8,14 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]:-$0}")
 
 function get_default_repo_branch() {
   # Check if main exists and use instead of master
-  command git rev-parse --git-dir &>/dev/null || return
-  local ref
-  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default}; do
-    if command git show-ref -q --verify "$ref"; then
-      echo "${ref##*/}"
-      return
-    fi
-  done
+  if command git rev-parse --git-dir &>/dev/null; then
+    for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default}; do
+      if command git show-ref -q --verify "$ref"; then
+        echo "${ref##*/}"
+        return
+      fi
+    done
+  fi
   echo master
 }
 
