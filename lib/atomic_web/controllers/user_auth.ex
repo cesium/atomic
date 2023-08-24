17a7169eb025ce01b264075530dcf3572f7632f8
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-08-25 00:10:35 +0100

    refactor: review when to show "you must login" notification

--- lib/atomic_web/controllers/user_auth.ex
+++ lib/atomic_web/controllers/user_auth.ex
@@ -100,7 +100,7 @@ defmodule AtomicWeb.UserAuth do
     conn
     |> renew_session()
     |> delete_resp_cookie(@remember_me_cookie)
-    |> redirect(to: "/")
+    |> redirect(to: Routes.user_session_path(conn, :new))
   end
 
   @doc """
