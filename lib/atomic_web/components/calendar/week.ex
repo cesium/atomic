abf6bde63bc89dbc16ef36cca77c4f7becf99fc9
Rui Lopes <76881129+RuiL1904@users.noreply.github.com>
2023-09-10 21:42:38 +0100

    feat: feed first steps

--- lib/atomic_web/components/calendar/week.ex
+++ lib/atomic_web/components/calendar/week.ex
@@ -111,10 +111,10 @@ defmodule AtomicWeb.Components.CalendarWeek do
         <li class={"#{col_start(@idx + 1)} relative mt-px flex"} style={"grid-row: #{calc_row_start(activity.start)} / span #{calc_time(activity.start, activity.finish)}"}>
           <%= live_patch to: Routes.activity_show_path(AtomicWeb.Endpoint, :show, activity, assigns.organization) do %>
             <div class="group absolute inset-1 flex flex-col overflow-y-auto rounded-lg bg-orange-50 p-2 text-xs leading-5 hover:bg-orange-100">
-              <p class="order-1 font-semibold text-orange-600">
+              <p class="order-1 font-semibold text-orange-500">
                 activity.title
               </p>
-              <p class="text-orange-600 group-hover:text-orange-800">
+              <p class="text-orange-500 group-hover:text-orange-800">
                 <time datetime={activity.start}><%= Calendar.strftime(activity.start, "%Hh%M") %></time>
               </p>
             </div>
