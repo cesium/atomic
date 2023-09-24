e8a33fd1a9f16e03ba5796ca650eaaed3098fdd0
João Lobo <30907944+joaodiaslobo@users.noreply.github.com>
2023-09-24 18:06:41 +0100

    feat: enhance reset password pages (#437)

---
+++ lib/atomic_web/controllers/user_change_password_controller.ex
@@ -0,0 +1,34 @@
+defmodule AtomicWeb.UserChangePasswordController do
+  use AtomicWeb, :controller
+
+  alias Atomic.Accounts
+  alias AtomicWeb.UserAuth
+
+  plug :assign_password_changeset
+
+  def edit(conn, _params) do
+    render(conn, "edit.html", error_message: nil)
+  end
+
+  def update(conn, %{"user" => user_params}) do
+    user = conn.assigns.current_user
+
+    case Accounts.update_user_password(user, user_params["current_password"], user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Password updated successfully.")
+        |> put_session(:user_return_to, Routes.user_change_password_path(conn, :edit))
+        |> UserAuth.log_in_user(user)
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, error_message: "Password didn't change.")
+    end
+  end
+
+  defp assign_password_changeset(conn, _opts) do
+    user = conn.assigns.current_user
+
+    conn
+    |> assign(:changeset, Accounts.change_user_password(user))
+  end
+end
