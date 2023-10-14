b2efddf1d4c9e60d0f7e7afb9d9102489824d42e
João Lobo <joao@lobo.is>
2023-10-14 17:32:32 +0100

    Make document generation generic

--- lib/atomic_web/controllers/data_export_controller.ex
+++ lib/atomic_web/controllers/data_export_controller.ex
@@ -4,6 +4,24 @@ defmodule AtomicWeb.DataExportController do
   alias Atomic.Organizations
   alias Elixlsx.{Sheet, Workbook}
 
+  # Generic excel heder styling
+  @header_styles [
+    align_horizontal: :center,
+    bold: true,
+    size: 12,
+    color: "#ffffff",
+    bg_color: "#ff9900"
+  ]
+
+  # Membership columns used on data generation, the column width sizes are only used on the excel export
+  @membership_export_columns [
+    [[:number], 10],
+    [[:user, :name], 40],
+    [[:user, :phone_number], 20],
+    [[:user, :email], 30],
+    [[:user, :inserted_at], 30]
+  ]
+
   @doc """
   Returns an organization's memberships as a CSV file.
   """
@@ -41,70 +59,68 @@ defmodule AtomicWeb.DataExportController do
     end
   end
 
+  defp write_memberships_csv(organization_id) do
+    Organizations.list_memberships(%{"organization_id" => organization_id}, [:user])
+    |> entities_to_csv(
+      @membership_export_columns
+      |> Enum.map(fn column -> List.first(column) end)
+    )
+  end
+
   defp write_memberships_xlsx(organization_id) do
     memberships = Organizations.list_memberships(%{"organization_id" => organization_id}, [:user])
     organization = Organizations.get_organization!(organization_id)
 
-    columns = [
-      [:number],
-      [:user, :name],
-      [:user, :phone_number],
-      [:user, :email],
-      # [:user, :inserted_at]
-    ]
-
-    header_styles = [
-      align_horizontal: :center,
-      bold: true,
-      size: 12,
-      color: "#ffffff",
-      bg_color: "#ff9900"
-    ]
-
-    memberships_sheet =
-      %Sheet{
-        name: "#{organization.name}'s Memberships",
-        rows:
-          [
-            Enum.map(columns, fn column ->
-              [column |> List.last() |> format_atom() | header_styles]
-            end)
-          ] ++
-            Enum.map(memberships, fn membership ->
-              Enum.map(columns, fn col ->
-                List.foldl(col, membership, fn key, entity -> Map.get(entity, key) end)
-              end)
-            end)
-      }
-      |> Sheet.set_pane_freeze(1, 5)
-      |> Sheet.set_col_width("A", 10)
-      |> Sheet.set_col_width("B", 40)
-      |> Sheet.set_col_width("C", 20)
-      |> Sheet.set_col_width("D", 30)
-      |> Sheet.set_col_width("E", 30)
-
-    case %Workbook{sheets: [memberships_sheet]}
+    case entities_to_xlsx_workbook(
+           memberships,
+           @membership_export_columns,
+           @header_styles,
+           "#{organization.name}'s Memberships"
+         )
          |> Elixlsx.write_to_memory("memberships-#{organization_id}.xlsx") do
       {:ok, {_, data}} -> {:ok, data}
       {:error, reason} -> {:error, to_string(reason)}
     end
   end
 
-  defp write_memberships_csv(organization_id) do
-    columns = [
-      [:number],
-      [:user, :name],
-      [:user, :phone_number],
-      [:user, :email],
-      [:user, :inserted_at]
-    ]
+  defp entities_to_xlsx_workbook(entities, columns, header_styles, sheet_name) do
+    paths = columns |> Enum.map(fn column -> List.first(column) end)
+    sizes = columns |> Enum.map(fn column -> List.last(column) end)
 
+    column_widths =
+      1..length(sizes)
+      |> Enum.zip(sizes)
+      |> Map.new()
+
+    sheet =
+      %Sheet{
+        name: sheet_name,
+        rows:
+          [
+            Enum.map(paths, fn column ->
+              [column |> List.last() |> format_atom() | header_styles]
+            end)
+          ] ++
+            Enum.map(entities, fn entity ->
+              Enum.map(paths, fn col ->
+                List.foldl(col, entity, fn key, object -> Map.get(object, key) end)
+                |> value_to_excel()
+              end)
+            end)
+      }
+      |> Sheet.set_pane_freeze(1, length(columns))
+      |> Map.replace(:col_widths, column_widths)
+
+    %Workbook{sheets: [sheet]}
+  end
+
+  defp entities_to_csv(entities, columns) do
     data =
       ([Enum.map(columns, fn col -> List.last(col) end) |> Enum.join(",")] ++
-         (Organizations.list_memberships(%{"organization_id" => organization_id}, [:user])
-          |> Enum.map(fn membership ->
+         (entities
+          |> Enum.map(fn entity ->
             Enum.map(columns, fn col ->
-              List.foldl(col, membership, fn key, entity -> Map.get(entity, key) end)
+              List.foldl(col, entity, fn key, object -> Map.get(object, key) end)
             end)
             |> Enum.join(",")
           end)))
@@ -115,12 +131,19 @@ defmodule AtomicWeb.DataExportController do
   end
 
   defp format_atom(key) do
-    IO.inspect(key)
     key
     |> to_string()
     |> String.split("_")
     |> Enum.map(&String.capitalize/1)
     |> Enum.join(" ")
+  end
+
+  defp value_to_excel(date = %NaiveDateTime{}) do
+    [datetime_to_excel(date), datetime: true]
+  end
+
+  defp value_to_excel(value) do
+    value
   end
 
   defp datetime_to_excel(date) do
