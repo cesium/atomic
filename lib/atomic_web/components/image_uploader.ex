ac036829cb61b652855cb8e1de88b619960d2614
Rui Lopes <ruilopesm19@gmail.com>
2024-02-17 19:29:47 +0000

    feat: page layout component

--- lib/atomic_web/components/image_uploader.ex
+++ lib/atomic_web/components/image_uploader.ex
@@ -30,7 +30,7 @@ defmodule AtomicWeb.Components.ImageUploader do
                   <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
                 <div class="flex text-sm text-gray-600">
-                  <label for="file-upload" class="relative cursor-pointer rounded-md font-medium text-orange-600 hover:text-red-800">
+                  <label for="file-upload" class="relative cursor-pointer rounded-md font-medium text-orange-500 hover:text-red-800">
                     <a onclick={"document.getElementById('#{@uploads.image.ref}').click()"}>
                       Upload a file
                     </a>
