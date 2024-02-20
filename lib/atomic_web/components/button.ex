b3f837adef90a690565167d97e932ae9915b8878
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-02-20 16:55:55 +0000

    fix: fixed identation and removed unnecessary parameter

--- lib/atomic_web/components/button.ex
+++ lib/atomic_web/components/button.ex
@@ -3,7 +3,9 @@ defmodule AtomicWeb.Components.Button do
   use AtomicWeb, :component
 
   attr :url, :string, required: true
+
   slot :inner_block, required: true
+
   def primary_button(assigns) do
     ~H"""
     <.link patch={@url} class="inline-flex items-center rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-500">
