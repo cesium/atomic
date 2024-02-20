9b76bedaed3d42ae3feb6c511b793728de657321
João Lobo <joao@lobo.is>
2024-02-20 12:06:45 +0000

    feat: replace items with component

--- lib/atomic_web/components/avatar.ex
+++ lib/atomic_web/components/avatar.ex
@@ -2,7 +2,7 @@ defmodule AtomicWeb.Components.Avatar do
   @moduledoc false
   use AtomicWeb, :component
 
-  attr :src, :string, required: true
+  attr :src, :string, default: nil
   attr :name, :string, required: true
   attr :type, :atom, values: [:user, :company], default: :user
   attr :size, :atom, values: [:xs, :sm, :md, :lg, :xl], default: :md
@@ -22,22 +22,28 @@ defmodule AtomicWeb.Components.Avatar do
   end
 
   defp generate_classes(assigns) do
-    "flex shrink-0 items-center justify-center #{classes(:type, assigns)} #{classes(:size, assigns)} #{classes(:color, assigns)} #{assigns.class}"
+    "flex shrink-0 items-center justify-center select-none #{classes(:type, assigns)} #{classes(:size, assigns)} #{classes(:color, assigns)} #{assigns.class}"
   end
 
-  defp classes(:type, %{type: :user}), do: "rounded-xl"
+  defp classes(:type, %{type: :user}), do: "rounded-full"
 
   defp classes(:type, %{type: :company}), do: "rounded-lg"
 
-  defp classes(:size, %{size: :xs}), do: "h-8 w-8"
+  defp classes(:size, %{size: :xs}), do: "h-8 w-8 text-xs font-medium leading-none"
 
   defp classes(:size, %{size: :sm}), do: "h-12 w-12 text-lg font-medium leading-none"
 
-  defp classes(:size, %{size: :md}), do: "h-16 w-16"
+  defp classes(:size, %{size: :md}), do: "h-16 w-16 text-lg font-medium leading-none"
 
-  defp classes(:size, %{size: :lg}), do: "h-20 w-20"
+  defp classes(:size, %{size: :lg}), do: "h-20 w-20 text-4xl font-medium leading-none"
 
   defp classes(:size, %{size: :xl}), do: "h-24 w-24 text-4xl font-medium leading-none"
 
-  defp classes(:color, %{color: color}), do: "bg-#{color}"
+  defp classes(:color, %{color: color, src: image}),
+    do:
+      (if image do
+         ""
+       else
+         "bg-#{color}"
+       end)
 end
