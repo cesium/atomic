8f8e2d85a10e6d055321cae48766f2bbba77fc1a
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-06-10 15:46:57 +0100

    somethin that I did long ago and I need to commit to change to develop

--- lib/atomic_web/components/activity.ex
+++ lib/atomic_web/components/activity.ex
@@ -15,7 +15,7 @@ defmodule AtomicWeb.Components.Activity do
     <div>
       <div class="flex">
         <div class="my-auto flex-shrink-0">
-          <.popover type={:organization} organization={@activity.organization} position={:bottom}>
+          <.popover type={:organization} organization={@activity.organization} position={:left}>
             <:wrapper>
               <.avatar name={@activity.organization.name} color={:light_gray} class="!h-10 !w-10" size={:xs} type={:organization} src={Uploaders.Logo.url({@activity.organization.logo, @activity.organization}, :original)} />
             </:wrapper>
