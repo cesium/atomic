fcc09e6bae56747d68f62720c19248234c6f88ba
Nuno Miguel <nmpf.2005@gmail.com>
2024-08-08 02:58:18 +0100

    refactor: improve calendar performance and style

--- lib/atomic_web/components/calendar/month.ex
+++ lib/atomic_web/components/calendar/month.ex
@@ -6,7 +6,7 @@ defmodule AtomicWeb.Components.CalendarMonth do
   import AtomicWeb.Components.Badge
 
   attr :id, :string, default: "calendar-month", required: false
-  attr :current_path, :string, required: true
+  attr :current_date, :string, required: true
   attr :activities, :list, required: true
   attr :timezone, :string, required: true
   attr :beginning_of_month, :string, required: true
@@ -15,9 +15,9 @@ defmodule AtomicWeb.Components.CalendarMonth do
 
   def calendar_month(assigns) do
     ~H"""
-    <div id={@id} class="rounded-lg shadow ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
-      <div class="grid grid-cols-7 gap-px rounded-t-lg border-b border-zinc-300 bg-zinc-200 text-center text-xs font-semibold leading-6 text-zinc-700 lg:flex-none">
-        <div class="rounded-tl-lg bg-white py-2">
+    <div id={@id} class="shadow lg:flex lg:flex-auto lg:flex-col">
+      <div class="grid grid-cols-7 gap-px border-b border-zinc-300 bg-zinc-200 text-center text-xs font-semibold leading-6 text-zinc-700 lg:flex-none">
+        <div class="bg-white py-2">
           M<span class="sr-only sm:not-sr-only">on</span>
         </div>
         <div class="bg-white py-2">
@@ -35,20 +35,21 @@ defmodule AtomicWeb.Components.CalendarMonth do
         <div class="bg-white py-2">
           S<span class="sr-only sm:not-sr-only">at</span>
         </div>
-        <div class="rounded-tr-lg bg-white py-2">
+        <div class="bg-white py-2">
           S<span class="sr-only sm:not-sr-only">un</span>
         </div>
       </div>
       <div class="flex bg-zinc-200 text-xs leading-6 text-zinc-700 lg:flex-auto">
-        <div class="grid w-full grid-cols-7 gap-px overflow-hidden rounded-b-lg">
-          <%= for i <- 0..@end_of_month.day - 1 do %>
-            <.day index={i} params={@params} current_path={@current_path} activities={@activities} date={Timex.shift(@beginning_of_month, days: i)} timezone={@timezone} />
+        <div class="grid w-full grid-cols-7 grid-rows-6 gap-px overflow-hidden">
+          <%= for date <- generate_days_list(@beginning_of_month, @end_of_month) do %>
+            <%!-- <p><%= @end_of_month %></p> --%>
+            <.day index={date.day} params={@params} date={date} current_date={@current_date} activities={@activities} timezone={@timezone} />
           <% end %>
         </div>
       </div>
     </div>
     <div class="py-4 lg:hidden">
-      <ol class="divide-y divide-zinc-200 overflow-hidden rounded-lg bg-white text-sm shadow ring-1 ring-black ring-opacity-5">
+      <ol class="divide-y divide-zinc-200 overflow-hidden rounded-lg bg-white text-sm shadow">
         <%= for activity <- get_date_activities(@activities, current_from_params(@timezone, @params)) do %>
           <.link patch={Routes.activity_show_path(AtomicWeb.Endpoint, :show, activity)}>
             <li class="group flex p-4 pr-6 focus-within:bg-zinc-50 hover:bg-zinc-50">
@@ -74,16 +75,40 @@ defmodule AtomicWeb.Components.CalendarMonth do
     """
   end
 
+  defp generate_days_list(beginning_of_month, end_of_month) do
+    days_from_last_month = Timex.weekday(beginning_of_month, :monday)
+
+    past_month =
+      for i <- 1..(days_from_last_month - 1) do
+        Timex.shift(Timex.shift(beginning_of_month, days: -days_from_last_month), days: i)
+      end
+
+    current_month =
+      for i <- 0..(end_of_month.day - 1) do
+        Timex.shift(beginning_of_month, days: i)
+      end
+
+    next_month =
+      for i <- 1..(42 - Timex.days_in_month(beginning_of_month) - days_from_last_month + 1) do
+        Timex.shift(end_of_month, days: i)
+      end
+
+    if days_from_last_month == 1 do
+      current_month ++ next_month
+    else
+      past_month ++ current_month ++ next_month
+    end
+  end
+
   defp day(%{index: index, date: date, timezone: timezone} = assigns) do
     weekday = Timex.weekday(date, :monday)
     today? = Timex.compare(date, Timex.today(timezone))
 
     class =
       class_list([
-        {"relative py-2 px-3 lg:min-h-[110px] lg:flex hidden", true},
-        {col_start(weekday), index == 0},
-        {"bg-white", today? >= 0},
-        {"bg-zinc-50 text-zinc-500", today? < 0}
+        {"relative py-2 px-3 lg:h-28 lg:flex flex-col hidden", true},
+        {"bg-white", assigns.current_date.month == date.month},
+        {"bg-zinc-50 text-zinc-500", assigns.current_date.month != date.month}
       ])
 
     assigns =
@@ -99,25 +124,30 @@ defmodule AtomicWeb.Components.CalendarMonth do
     <div class={@class}>
       <time date-time={@date} class={
           "ml-auto lg:ml-0 pr-2 lg:pr-0 #{if @today? == 0 do
-            "flex h-6 w-6 items-center justify-center rounded-full bg-orange-400 font-semibold text-white shrink-0"
+            "flex h-6 w-6 items-center justify-center rounded-full bg-primary-600 font-semibold text-white"
           end}"
         }>
         <%= @text %>
       </time>
-      <ol class="mt-3 w-full">
-        <%= for activity <- get_date_activities(@activities, @date) do %>
+      <ol class="mt-2 -space-y-1">
+        <%= for activity <- get_date_activities(@activities, @date) |> Enum.take(2) do %>
           <li>
             <.link patch={Routes.activity_show_path(AtomicWeb.Endpoint, :show, activity)} class="group flex">
-              <p class="flex-auto truncate font-medium text-zinc-600 group-hover:text-zinc-800">
+              <p class="flex-auto truncate font-medium text-zinc-900 group-hover:text-primary-600">
                 <%= activity.title %>
               </p>
-              <time datetime={activity.start} class="mx-2 hidden flex-none text-zinc-600 group-hover:text-zinc-800 xl:block"><%= Calendar.strftime(activity.start, "%Hh") %></time>
+              <time datetime={activity.start} class="ml-3 hidden flex-none text-zinc-500 group-hover:text-primary-600 xl:block"><%= Calendar.strftime(activity.start, "%Hh") %></time>
             </.link>
+          </li>
+        <% end %>
+        <%= if Enum.count(get_date_activities(@activities, @date)) > 2 do %>
+          <li class="text-zinc-500">
+            + <%= Enum.count(get_date_activities(@activities, @date)) - 2 %> more
           </li>
         <% end %>
       </ol>
     </div>
-    <.link patch={build_path(@current_path, %{mode: "month", day: date_to_day(@date), month: date_to_month(@date), year: date_to_year(@date)})} class={"#{if @index == 0 do col_start(@weekday) end} min-h-[56px] flex w-full flex-col bg-white px-3 py-2 text-zinc-900 hover:bg-zinc-100 focus:z-10 lg:hidden"}>
+    <.link patch={build_path("", %{mode: "month", day: date_to_day(@date), month: date_to_month(@date), year: date_to_year(@date)})} class={"#{if @index == 0 do col_start(@weekday) end} min-h-[56px] flex w-full flex-col bg-white px-3 py-2 text-zinc-900 hover:bg-zinc-100 focus:z-10 lg:hidden"}>
       <time
         date-time={@date}
         class={
--- lib/atomic_web/live/calendar_live/show.ex
+++ lib/atomic_web/live/calendar_live/show.ex
@@ -3,9 +3,12 @@ defmodule AtomicWeb.CalendarLive.Show do
   use AtomicWeb, :live_view
 
   import AtomicWeb.CalendarUtils
-  import AtomicWeb.Components.Calendar
+  import AtomicWeb.Components.CalendarMonth
+  import AtomicWeb.Components.CalendarWeek
+  import AtomicWeb.Components.Dropdown
 
   alias Atomic.Activities
+  alias Timex.Duration
 
   @impl true
   def mount(_params, _session, socket) do
@@ -22,7 +25,95 @@ defmodule AtomicWeb.CalendarLive.Show do
      |> assign(:current_page, :calendar)
      |> assign(:params, params)
      |> assign(:mode, mode)
-     |> assign(:activities, list_activities(socket.assigns.timezone, mode, params))}
+     |> assign(:activities, list_activities(socket.assigns.timezone, mode, params))
+     |> assign(:current_path, Routes.calendar_show_path(AtomicWeb.Endpoint, :show))
+     |> assign(:current_date, Timex.today(socket.assigns.timezone))
+     |> assigns_month(
+       Routes.calendar_show_path(AtomicWeb.Endpoint, :show),
+       socket.assigns.timezone,
+       params
+     )}
+  end
+
+  @impl true
+  def handle_event("previous", _, socket) do
+    {:noreply,
+     socket
+     |> assign(
+       :current_date,
+       socket.assigns.beginning_of_month |> Timex.add(Duration.from_days(-1))
+     )
+     |> assigns_month(
+       Routes.calendar_show_path(AtomicWeb.Endpoint, :show),
+       socket.assigns.timezone,
+       socket.assigns.params
+     )}
+  end
+
+  @impl true
+  def handle_event("present", _, socket) do
+    {:noreply,
+     socket
+     |> assign(:current_date, Timex.today(socket.assigns.timezone))
+     |> assigns_month(
+       Routes.calendar_show_path(AtomicWeb.Endpoint, :show),
+       socket.assigns.timezone,
+       socket.assigns.params
+     )}
+  end
+
+  @impl true
+  def handle_event("next", _, socket) do
+    {:noreply,
+     socket
+     |> assign(:current_date, socket.assigns.end_of_month |> Timex.add(Duration.from_days(1)))
+     |> assigns_month(
+       Routes.calendar_show_path(AtomicWeb.Endpoint, :show),
+       socket.assigns.timezone,
+       socket.assigns.params
+     )}
+  end
+
+  defp assigns_month(socket, current_path, timezone, params) do
+    current = socket.assigns.current_date
+    beginning_of_month = Timex.beginning_of_month(current)
+    end_of_month = Timex.end_of_month(current)
+
+    last_day_previous_month =
+      beginning_of_month
+      |> Timex.add(Duration.from_days(-1))
+
+    first_day_next_month =
+      end_of_month
+      |> Timex.add(Duration.from_days(1))
+
+    previous_month =
+      last_day_previous_month
+      |> date_to_month()
+
+    next_month =
+      first_day_next_month
+      |> date_to_month()
+
+    previous_month_year =
+      last_day_previous_month
+      |> date_to_year()
+
+    next_month_year =
+      first_day_next_month
+      |> date_to_year()
+
+    previous_month_path =
+      build_path(current_path, %{mode: "month", month: previous_month, year: previous_month_year})
+
+    next_month_path =
+      build_path(current_path, %{mode: "month", month: next_month, year: next_month_year})
+
+    socket
+    |> assign(beginning_of_month: beginning_of_month)
+    |> assign(end_of_month: end_of_month)
+    |> assign(previous_month_path: previous_month_path)
+    |> assign(next_month_path: next_month_path)
   end
 
   defp list_activities(timezone, mode, params) do
