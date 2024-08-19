0d03f8742114cc532614494c05818f5baa543b39
João Lobo <joaodlobo@gmail.com>
2024-08-19 18:05:17 +0100

    refactor: uploaders

--- lib/atomic/uploaders/partner_image.ex
+++ lib/atomic/uploaders/partner_image.ex
@@ -2,11 +2,17 @@ defmodule Atomic.Uploaders.PartnerImage do
   @moduledoc """
   Uploader for partner images.
   """
-  use Atomic.Uploader
+  use Atomic.Uploader, extensions: ~w(.jpg .jpeg .png)
 
   alias Atomic.Organizations.Partner
 
-  def storage_dir(_version, {_file, %Partner{} = scope}) do
-    "uploads/atomic/partners/#{scope.id}"
+  @versions [:original]
+
+  def storage_dir(_version, {_file, %Partner{} = partner}) do
+    "uploads/atomic/partners/#{partner.id}/logo"
+  end
+
+  def filename(version, _) do
+    version
   end
 end
--- lib/atomic/uploaders/post.ex
+++ lib/atomic/uploaders/post.ex
@@ -2,16 +2,22 @@ defmodule Atomic.Uploaders.Post do
   @moduledoc """
   Uploader for posts.
   """
-  use Atomic.Uploader
+  use Atomic.Uploader, extensions: ~w(.jpg .jpeg .png .svg)
 
   alias Atomic.Activities.Activity
   alias Atomic.Organizations.Announcement
 
-  def storage_dir(_version, {_file, %Activity{} = scope}) do
-    "uploads/atomic/activities/#{scope.id}"
+  @versions [:original]
+
+  def storage_dir(_version, {_file, %Activity{} = activity}) do
+    "uploads/atomic/activities/#{activity.id}/image"
   end
 
-  def storage_dir(_version, {_file, %Announcement{} = scope}) do
-    "uploads/atomic/announcements/#{scope.id}"
+  def storage_dir(_version, {_file, %Announcement{} = announcement}) do
+    "uploads/atomic/announcements/#{announcement.id}/image"
+  end
+
+  def filename(version, _) do
+    version
   end
 end
