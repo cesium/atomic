783805fca80c72802bd621f7b66366c527b87072
Nuno Miguel <30806591+nunom27@users.noreply.github.com>
2024-09-19 19:11:16 +0100

    refactor: improve calendar performance and style (#518)

---
+++ lib/atomic_web/live/calendar_live/components/month.ex
@@ -0,0 +1,195 @@
+defmodule AtomicWeb.CalendarLive.Components.CalendarMonth do
+  @moduledoc false
+  use AtomicWeb, :component
+
+  import AtomicWeb.CalendarLive.Components.CalendarUtils
+
+  attr :id, :string, default: "calendar-month", required: false
+  attr :current_date, :string, required: true
+  attr :activities, :list, required: true
+  attr :timezone, :string, required: true
+  attr :beginning_of_month, :string, required: true
+  attr :end_of_month, :string, required: true
+  attr :params, :map, required: true
+
+  def calendar_month(assigns) do
+    ~H"""
+    <div id={@id} class="shadow ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
+      <div class="grid grid-cols-7 gap-px border-b border-zinc-300 bg-zinc-200 text-center text-xs font-semibold leading-6 text-zinc-700 lg:flex-none">
+        <div class="bg-white py-2">
+          M<span class="sr-only sm:not-sr-only">on</span>
+        </div>
+        <div class="bg-white py-2">
+          T<span class="sr-only sm:not-sr-only">ue</span>
+        </div>
+        <div class="bg-white py-2">
+          W<span class="sr-only sm:not-sr-only">ed</span>
+        </div>
+        <div class="bg-white py-2">
+          T<span class="sr-only sm:not-sr-only">hu</span>
+        </div>
+        <div class="bg-white py-2">
+          F<span class="sr-only sm:not-sr-only">ri</span>
+        </div>
+        <div class="bg-white py-2">
+          S<span class="sr-only sm:not-sr-only">at</span>
+        </div>
+        <div class="bg-white py-2">
+          S<span class="sr-only sm:not-sr-only">un</span>
+        </div>
+      </div>
+      <div class="flex bg-zinc-200 text-xs leading-6 text-zinc-700 lg:flex-auto">
+        <div class="grid w-full grid-cols-7 grid-rows-6 gap-px overflow-hidden">
+          <.day :for={date <- generate_days_list(@beginning_of_month, @end_of_month)} params={@params} date={date} current_date={@current_date} activities={@activities} timezone={@timezone} />
+        </div>
+      </div>
+    </div>
+    <%= if length(get_date_activities(@activities, @current_date)) > 0 do %>
+      <div class="px-4 py-10 sm:px-6 lg:hidden">
+        <ol class="divide-y divide-zinc-100 overflow-hidden rounded-lg bg-white text-sm shadow ring-1 ring-black ring-opacity-5">
+          <li :for={activity <- get_date_activities(@activities, @current_date)}>
+            <.link patch={Routes.activity_show_path(AtomicWeb.Endpoint, :show, activity)} class="group flex justify-between p-4 pr-6 focus-within:bg-gray-50 hover:bg-gray-50">
+              <div class="flex-auto">
+                <p class="font-semibold text-zinc-900">
+                  <%= activity.title %>
+                </p>
+                <div class="flex flex-row items-center gap-x-2 pt-2">
+                  <time datetime={activity.start} class="mt-2 flex items-center text-zinc-700">
+                    <.icon name="hero-clock-solid" class="size-5 mr-2 text-zinc-400" />
+                    <%= Calendar.strftime(activity.start, "%Hh%M") %>
+                  </time>
+                </div>
+              </div>
+            </.link>
+          </li>
+        </ol>
+      </div>
+    <% end %>
+    """
+  end
+
+  # Generates a list of days for the calendar month,
+  # including days from the previous and next months to fill the 6x7 grid.
+  #
+  # Example
+  #
+  # iex> generate_days_list(~D[2024-09-01], ~D[2024-09-30])
+  # [
+  # ~D[2024-08-26],
+  # ~D[2024-08-27],
+  # ~D[2024-08-28],
+  # ~D[2024-08-29],
+  # ~D[2024-08-30],
+  # ~D[2024-08-31],
+  # ~D[2024-09-01],
+  # ~D[2024-09-02],
+  # ...,
+  # ~D[2024-09-29],
+  # ~D[2024-09-30],
+  # ~D[2024-10-01],
+  # ~D[2024-10-02],
+  # ~D[2024-10-03],
+  # ~D[2024-10-04],
+  # ~D[2024-10-05],
+  # ~D[2024-10-06]
+  # ]
+
+  defp generate_days_list(beginning_of_month, end_of_month) do
+    days_from_last_month = Timex.weekday(beginning_of_month, :monday)
+
+    past_month =
+      for i <- 1..(days_from_last_month - 1) do
+        Timex.shift(Timex.shift(beginning_of_month, days: -days_from_last_month), days: i)
+      end
+
+    current_month =
+      for i <- 0..(end_of_month.day - 1) do
+        Timex.shift(beginning_of_month, days: i)
+      end
+
+    next_month =
+      for i <- 1..(42 - Timex.days_in_month(beginning_of_month) - days_from_last_month + 1) do
+        Timex.shift(end_of_month, days: i)
+      end
+
+    if days_from_last_month == 1 do
+      current_month ++ next_month
+    else
+      past_month ++ current_month ++ next_month
+    end
+  end
+
+  defp day(%{date: date, timezone: timezone} = assigns) do
+    today? = Timex.compare(date, Timex.today(timezone))
+
+    assigns =
+      assigns
+      |> assign(:text, Timex.format!(date, "{D}"))
+      |> assign(:date, date)
+      |> assign(:today?, today?)
+
+    ~H"""
+    <div class={[
+      "relative py-2 px-3 lg:h-28 lg:flex flex-col hidden",
+      @current_date.month == @date.month && "bg-white",
+      @current_date.month != @date.month && "bg-zinc-50 text-zinc-500"
+    ]}>
+      <time date-time={@date} class={
+          "ml-auto lg:ml-0 pr-2 lg:pr-0 #{if @today? == 0 do
+            "flex size-6 items-center justify-center rounded-full bg-primary-600 font-semibold text-white"
+          end}"
+        }>
+        <%= @text %>
+      </time>
+      <ol class="mt-2 -space-y-1">
+        <li :for={activity <- get_date_activities(@activities, @date) |> Enum.take(2)}>
+          <.link patch={Routes.activity_show_path(AtomicWeb.Endpoint, :show, activity)} class="group flex">
+            <p class="flex-auto truncate font-medium text-zinc-900 group-hover:text-primary-600">
+              <%= activity.title %>
+            </p>
+            <time datetime={activity.start} class="ml-3 hidden flex-none text-zinc-500 group-hover:text-primary-600 xl:block"><%= Calendar.strftime(activity.start, "%Hh") %></time>
+          </.link>
+        </li>
+        <li :if={Enum.count(get_date_activities(@activities, @date)) > 2} class="text-zinc-500 hover:text-primary-600">
+          <button type="button" phx-click="show-more" phx-value-date={@date}>
+            +<%= Enum.count(get_date_activities(@activities, @date)) - 2 %> more
+          </button>
+        </li>
+      </ol>
+    </div>
+    <.link
+      phx-click="set-current-date"
+      phx-value-date={@date}
+      class={[
+        "min-h-[56px] flex w-full flex-col bg-white px-3 py-2 text-zinc-900 focus:z-10 lg:hidden",
+        @current_date.month == @date.month && "hover:bg-zinc-100",
+        @current_date.month != @date.month && "bg-zinc-50"
+      ]}
+    >
+      <time
+        date-time={@date}
+        class={[
+          "ml-auto lg:ml-0",
+          @current_date == @date && "flex size-6 items-center justify-center rounded-full text-white shrink-0",
+          @current_date == @date && @today? == 0 && "bg-primary-600",
+          @current_date == @date && @today? != 0 && "bg-zinc-900",
+          @current_date != @date && @today? == 0 && "text-primary-700",
+          @current_date != @date && @date.month != @current_date.month && "text-zinc-500"
+        ]}
+      >
+        <%= @text %>
+      </time>
+      <%= if (activities = get_date_activities(@activities, @date)) != [] do %>
+        <span class="sr-only"><%= Enum.count(activities) %> events</span>
+        <span class="-mx-0.5 mt-auto flex flex-wrap-reverse">
+          <%= for activity <- Enum.take(activities, 3) do %>
+            <%= if activity do %>
+              <span class="mx-0.5 mb-1 h-1.5 w-1.5 rounded-full bg-zinc-400"></span>
+            <% end %>
+          <% end %>
+        </span>
+      <% end %>
+    </.link>
+    """
+  end
+end
---
+++ lib/atomic_web/live/calendar_live/components/week.ex
@@ -0,0 +1,264 @@
+defmodule AtomicWeb.CalendarLive.Components.CalendarWeek do
+  @moduledoc false
+  use AtomicWeb, :component
+
+  alias Timex.Duration
+
+  import AtomicWeb.CalendarLive.Components.CalendarUtils
+
+  attr :id, :string, default: "calendar-week", required: false
+  attr :current_date, :string, required: true
+  attr :activities, :list, required: true
+  attr :timezone, :string, required: true
+  attr :beginning_of_week, :string, required: true
+  attr :end_of_week, :string, required: true
+  attr :params, :map, required: true
+
+  def calendar_week(%{timezone: timezone} = assigns) do
+    days_of_week =
+      for idx <- 0..6 do
+        day_of_week = assigns.beginning_of_week |> Timex.add(Duration.from_days(idx))
+        day_of_week_mobile = Enum.at(["M", "T", "W", "T", "F", "S", "S"], idx)
+        {day_of_week, day_of_week_mobile}
+      end
+
+    assigns =
+      assigns
+      |> assign(week: ["Mon ", "Tue ", "Wed ", "Thu ", "Fri ", "Sat ", "Sun "])
+      |> assign(today: Timex.today(timezone))
+      |> assign(days_of_week: days_of_week)
+
+    ~H"""
+    <div id={@id} class="isolate flex flex-auto flex-col overflow-y-auto rounded-lg bg-white">
+      <div style="width: 165%" class="flex max-w-full flex-none flex-col sm:max-w-none md:max-w-full">
+        <div class="sticky top-0 z-30 flex-none bg-white shadow ring-1 ring-black ring-opacity-5 sm:pr-8">
+          <div class="grid grid-cols-7 text-sm leading-6 text-zinc-500 sm:hidden">
+            <%= for {day_of_week, day_of_week_mobile} <- @days_of_week do %>
+              <.link phx-click="set-current-date" phx-value-date={day_of_week} class="flex flex-col items-center py-2">
+                <%= day_of_week_mobile %>
+                <span class={[
+                  "flex items-center justify-center w-8 h-8 mt-1 font-semibold",
+                  @today == day_of_week && "bg-primary-700 rounded-full text-white",
+                  @today != day_of_week && day_of_week == @current_date && "bg-zinc-900 rounded-full text-white",
+                  @today != day_of_week && day_of_week != @current_date && "text-zinc-900"
+                ]}>
+                  <%= day_of_week |> date_to_day() %>
+                </span>
+              </.link>
+            <% end %>
+          </div>
+          <div class="-mr-px hidden grid-cols-7 divide-x divide-zinc-100 border-r border-zinc-100 text-sm leading-6 text-zinc-500 sm:grid">
+            <div class="col-end-1 w-14"></div>
+            <%= for idx <- 0..6 do %>
+              <% day_of_week = @beginning_of_week |> Timex.add(Duration.from_days(idx)) %>
+              <div id={"day-of-week-#{idx}"} class="flex h-12 items-center justify-center">
+                <span class={@today == day_of_week && "flex items-baseline"}>
+                  <%= Enum.at(@week, idx) %>
+                  <span class={[
+                    "items-center justify-center font-semibold",
+                    @today == day_of_week && "flex ml-1.5 w-8 h-8 text-white bg-primary-600 rounded-full",
+                    @today != day_of_week && "text-zinc-900"
+                  ]}>
+                    <%= day_of_week |> date_to_day() %>
+                  </span>
+                </span>
+              </div>
+            <% end %>
+          </div>
+        </div>
+        <div class="flex flex-auto">
+          <div class="sticky left-0 z-20 w-14 flex-none bg-white ring-1 ring-zinc-100"></div>
+          <div class="grid flex-auto grid-cols-1 grid-rows-1">
+            <!-- Horizontal lines -->
+            <div class="col-start-1 col-end-2 row-start-1 grid divide-y divide-zinc-100" style="grid-template-rows: repeat(48, minmax(3.5rem, 1fr))">
+              <div class="row-end-1 h-7"></div>
+              <%= for hour <- hours() do %>
+                <div>
+                  <div class="sticky left-0 z-20 -mt-2.5 -ml-14 w-14 pr-2 text-right text-xs leading-5 text-zinc-400"><%= hour %></div>
+                </div>
+                <div></div>
+              <% end %>
+            </div>
+            <!-- Vertical lines -->
+            <div class="col-start-1 col-end-2 row-start-1 hidden grid-cols-7 grid-rows-1 divide-x divide-zinc-100 sm:grid sm:grid-cols-7">
+              <div class="col-start-1 row-span-full"></div>
+              <div class="col-start-2 row-span-full"></div>
+              <div class="col-start-3 row-span-full"></div>
+              <div class="col-start-4 row-span-full"></div>
+              <div class="col-start-5 row-span-full"></div>
+              <div class="col-start-6 row-span-full"></div>
+              <div class="col-start-7 row-span-full"></div>
+              <div class="col-start-8 row-span-full w-8"></div>
+            </div>
+            <!-- Events -->
+            <ol class="col-start-1 col-end-2 row-start-1 grid grid-cols-1 sm:hidden" style="grid-template-rows: 1.75rem repeat(288, minmax(0, 1fr))">
+              <.day date={@current_date} idx={0} activities={@activities} />
+            </ol>
+            <ol class="col-start-1 col-end-2 row-start-1 hidden sm:grid sm:grid-cols-7 sm:pr-8" style="grid-template-rows: 1.75rem repeat(288, minmax(0, 1fr)) auto">
+              <%= for idx <- 0..6 do %>
+                <.day date={Timex.shift(@beginning_of_week, days: idx)} idx={idx} activities={@activities} />
+              <% end %>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  defp day(assigns) do
+    assigns =
+      assigns
+      |> assign_activities_positions(get_date_activities(assigns.activities, assigns.date))
+
+    ~H"""
+    <%= for {activity, width, left} <- @activities_with_positions do %>
+      <li class={"#{col_start(@idx + 1)} relative mt-px flex"} style={"
+            grid-row: #{calc_row_start(activity.start)} / span #{calc_time(activity.start, activity.finish)};
+            width: #{width}%;
+            left: #{left}%"}>
+        <.link patch={Routes.activity_show_path(AtomicWeb.Endpoint, :show, activity)}>
+          <div class={[
+            "group absolute inset-1 flex flex-col overflow-x-hidden rounded-md bg-primary-50 p-2 text-xs leading-5 hover:bg-primary-100 sm:overflow-y-hidden sm:hover:overflow-y-auto",
+            width != 100 && "sm:hover:z-10 sm:hover:w-max sm:max-w-[117px]"
+          ]}>
+            <p class="text-primary-500 order-1 font-semibold">
+              <%= activity.title %>
+            </p>
+            <p class="text-primary-500 group-hover:text-primary-800">
+              <time datetime={activity.start}><%= Calendar.strftime(activity.start, "%Hh%M") %></time>
+            </p>
+          </div>
+        </.link>
+      </li>
+    <% end %>
+    """
+  end
+
+  defp assign_activities_positions(assigns, day_activities) do
+    activities_with_positions =
+      Enum.map(day_activities, fn activity ->
+        # Calculate the width of the activity in percentage
+        width = 1 / (calc_total_overlaps(activity, day_activities) + 1) * 100
+
+        # Calculate the left position of the activity in percentage
+        left =
+          calc_left_amount(
+            activity,
+            day_activities,
+            calc_total_overlaps(activity, day_activities) + 1
+          )
+
+        {activity, width, left}
+      end)
+
+    assign(assigns, :activities_with_positions, activities_with_positions)
+  end
+
+  defp calc_row_start(start) do
+    hours =
+      start
+      |> Timex.format!("{h24}")
+      |> String.to_integer()
+
+    minutes =
+      start
+      |> Timex.format!("{m}")
+      |> String.to_integer()
+
+    hours * 12 + div(minutes, 5) + 2
+  end
+
+  # Each row spans a 5-minute interval.
+  # Calculates the number of grid rows the activity should
+  # span based on the time difference between the start and finish.
+  #
+  # Example
+  #
+  # iex> calc_time(~N[2024-09-11 09:00:00], ~N[2024-09-11 10:00:00])
+  # 12
+
+  defp calc_time(start, finish) do
+    time_diff = (NaiveDateTime.diff(finish, start) / 60) |> trunc()
+
+    if time_diff == 0 do
+      1
+    else
+      div(time_diff, 5)
+    end
+  end
+
+  # Calculates the total number of overlapping activities with the current activity on the same day.
+  #
+  # Example
+  #
+  # iex> activities = [
+  # ...>   %{start: ~N[2024-09-11 09:00:00], finish: ~N[2024-09-11 10:00:00]},
+  # ...>   %{start: ~N[2024-09-11 09:30:00], finish: ~N[2024-09-11 10:30:00]}
+  # ...> ]
+  # ...>
+  # ...> calc_total_overlaps(
+  # ...>   %{start: ~N[2024-09-11 09:00:00], finish: ~N[2024-09-11 10:00:00]},
+  # ...>   activities
+  # ...> )
+  # 1
+
+  def calc_total_overlaps(current_activity, activities) do
+    current_interval =
+      Timex.Interval.new(from: current_activity.start, until: current_activity.finish)
+
+    activities
+    |> Enum.filter(fn activity ->
+      activity_interval = Timex.Interval.new(from: activity.start, until: activity.finish)
+
+      activity != current_activity && activity.start.day == current_activity.start.day and
+        Timex.Interval.overlaps?(current_interval, activity_interval)
+    end)
+    |> length()
+  end
+
+  # Calculates the left offset percentage for positioning an activity on the calendar grid.
+  #
+  # The left offset is determined by the number of overlapping activities prior to the current one.
+  #
+  #
+  # Example
+  #
+  # iex> activities = [
+  # ...>   %{start: ~N[2024-09-11 09:00:00], finish: ~N[2024-09-11 10:00:00]},
+  # ...>   %{start: ~N[2024-09-11 09:30:00], finish: ~N[2024-09-11 10:30:00]}
+  # ...> ]
+  # ...>
+  # ...> calc_left_amount(
+  # ...>   %{start: ~N[2024-09-11 09:30:00], finish: ~N[2024-09-11 10:30:00]},
+  # ...>   activities,
+  # ...>   2
+  # ...> )
+  # 50.0
+  defp calc_left_amount(current_activity, activities, activity_total_overlaps) do
+    current_interval =
+      Timex.Interval.new(from: current_activity.start, until: current_activity.finish)
+
+    # Total number of overlaps prior to the current activity
+    total_overlaps =
+      activities
+      |> Enum.take_while(fn activity -> activity != current_activity end)
+      |> Enum.filter(fn activity ->
+        activity_interval = Timex.Interval.new(from: activity.start, until: activity.finish)
+
+        activity != current_activity && activity.start.day == current_activity.start.day and
+          Timex.Interval.overlaps?(current_interval, activity_interval)
+      end)
+      |> length()
+
+    if total_overlaps > 0 do
+      total_overlaps / activity_total_overlaps * 100
+    else
+      0
+    end
+  end
+
+  defp hours do
+    0..23 |> Enum.map(fn n -> "#{n}H" end)
+  end
+end
