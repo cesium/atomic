d973f50dfbd75dc3527a8c6b8134f17f9144e682
Afonso Martins <afonsopm2005@gmail.com>
2024-10-04 01:22:20 +0100

    fix: pass tests

--- lib/atomic_web/components/socials.ex
+++ lib/atomic_web/components/socials.ex
@@ -3,17 +3,15 @@ defmodule AtomicWeb.Components.Socials do
   use Phoenix.Component
 
   def socials(assigns) do
-    social_links = [
-      {:tiktok, "tiktok.svg", "https://tiktok.com/"},
-      {:instagram, "instagram.svg", "https://instagram.com/"},
-      {:facebook, "facebook.svg", "https://facebook.com/"},
-      {:x, "x.svg", "https://x.com/"}
-    ]
-
     ~H"""
     <%= if @entity.socials do %>
       <div class="mt-4 flex gap-4">
-        <%= for {social, icon, url_base} <- social_links do %>
+        <%= for {social, icon, url_base} <- [
+            {:tiktok, "tiktok.svg", "https://tiktok.com/"},
+            {:instagram, "instagram.svg", "https://instagram.com/"},
+            {:facebook, "facebook.svg", "https://facebook.com/"},
+            {:x, "x.svg", "https://x.com/"}
+          ] do %>
           <% social_value = Map.get(@entity.socials, social) %>
           <%= if social_value do %>
             <div class="flex flex-row items-center gap-x-1">
