3a9f93529f213819ec2fb3bec9127925fa5f2318
RicoPleasure <esprazj.dev@gmail.com>
2024-11-19 14:04:08 +0000

    feat: add link to home (mobile) and add smooth behavior

--- lib/atomic_web/live/legal_terms_live/components/header.ex
+++ lib/atomic_web/live/legal_terms_live/components/header.ex
@@ -6,13 +6,13 @@ defmodule AtomicWeb.LegalTermsLive.Components.Header do
     ~H"""
     <header class="w-full fixed flex bg-white place-items-center justify-between px-8 pt-8 pb-4">
         <div class="flex place-items-center gap-x-6 md:gap-x-12 lg:pl-12">
-            <div class="flex place-items-center gap-x-12 sm:gap-x-4">
+            <div class="flex place-items-center gap-x-4">
               <!-- Atomic Logo -->
               <.link navigate={~p"/"}>
                   <img src={~p"/images/atomic.svg"} class="h-14 w-auto" />
               </.link>
               <!-- Section Name -->
-              <p class="text-2xl font-semibold text-zinc-400 select-none"><%= @page_name %></p>
+              <p class="hidden sm:block text-2xl font-semibold text-zinc-400 select-none"><%= @page_name %></p>
             </div>
             <div class="hidden sm:block flex place-items-center gap-x-2 sm:gap-x-4 sm:space-x-2 md:space-x-4 text-zinc-300 font-semibold text-sm">
             <%= if @page_name == "Terms of Service" do %>
@@ -30,8 +30,9 @@ defmodule AtomicWeb.LegalTermsLive.Components.Header do
             </div>
         </div>
         <!-- Link to Home (hidden on mobile) -->
-        <div class="flex place-items-center hidden sm:block lg:pr-12">
-            <.link class="atomic-button atomic-button--white atomic-button--md" navigate={~p"/"}>Back home</.link>
+        <div class="flex place-items-center lg:pr-12">
+            <.link class="hidden sm:block atomic-button atomic-button--white atomic-button--md" navigate={~p"/"}>Back home</.link>
+            <.link class="block sm:hidden atomic-button atomic-button--md hero-home text-zinc-400" navigate={~p"/"}></.link>
         </div>
     </header>
     """
