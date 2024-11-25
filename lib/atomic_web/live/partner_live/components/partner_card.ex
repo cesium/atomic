942216f7bf9f9960a291ff0b57604863772feea2
faria-s <salome.faria8@gmail.com>
2024-11-25 23:49:04 +0000

    fix: router and icons

--- lib/atomic_web/live/partner_live/components/partner_card.ex
+++ lib/atomic_web/live/partner_live/components/partner_card.ex
@@ -8,35 +8,42 @@ defmodule AtomicWeb.PartnerLive.Components.PartnerCard do
 
   def partner_card(assigns) do
     ~H"""
-          <li class="gap-x-6 h-full w-full grid rounded-lg border border-zinc-200 hover:bg-zinc-50 ">
-            <div class="h-20 w-full">
-              <div class="h-16 w-full">
+          <li class="h-full w-full grid rounded-lg border border-zinc-200 hover:bg-zinc-50 ">
+            <div class="h-12 w-full">
+              <div class="h-full w-full">
                 <.gradient seed={@partner.id} class="rounded-t-lg" />
               </div> 
-              <div class="flex justify-center relative bottom-20">
-                <.avatar color={:light_gray} name={@partner.name} class="size-28 object-cover" src={Uploaders.PartnerImage.url({@partner.image, @partner}, :original)} type={:company} size={:sm} />
-              </div>
             </div>
-            <div class="flex flex-col mt-8 md:flex-row gap-x-4 w-full px-12">
-              <div class="flex-grow flex-col justify-center items-center">
-                <p class="text-md text-lg font-semibold leading-6 text-center text-zinc-900"><%= @partner.name %></p>
-                <%= if @partner.location do %>
-                  <div class="flex justify-center items-center gap-x-1 leading-6 z-1">
-                    <.icon name={:map_pin} class="h-4 w-4 my-1 text-zinc-400" />
-                    <p class="text-blue-400 text-center text-sm"><%= @partner.location.name %></p>
-                  </div>
-                <% end %>
-                <div>
-                  <p class="mt-5 mb-10 truncate text-xs leading-5 text-zinc-500 overflow-hidden overflow-ellipsis whitespace-normal">
-                    <%= Enum.map(String.split(@partner.benefits, "\n"), fn phrase -> %>
-                      <%= if String.length(phrase) < 75 do %>
-                        <%= phrase %><br />
-                      <% else %>
-                        <%= String.slice(phrase, 0..75) <> "..." %> <br />
-                      <% end %>
-                    <% end) %>
-                  </p>
+
+            <div class="grid w-full">
+              <div class="flex flex-grow px-6">
+                <div class="flex relative bottom-6">
+                  <.avatar color={:light_gray} class="" name={@partner.name} src={Uploaders.PartnerImage.url({@partner.image, @partner}, :original)} type={:company} size={:xl} />
                 </div>
+                <div class="px-4 mt-5">
+                  <div class="relative group ">
+                    <p class="text-md text-lg line-clamp-1 font-semibold leading-6 text-zinc-900"><%= @partner.name %></p>
+                    <div class="absolute top-full w-max left-14 mt-2 hidden rounded bg-gray-500 text-white text-sm p-1 group-hover:block opacity-100 transition-opacity duration-300"><%= @partner.name %></div>
+                    <div class="absolute top-full left-16 mt-1 hidden group-hover:block border-l-4 border-r-4 border-b-4 border-l-transparent border-r-transparent border-b-gray-500"></div>
+                  </div>
+                  <%= if @partner.location do %>
+                    <div class="flex items-center gap-x-1 leading-6 z-1">
+                      <.icon name="hero-map-pin" class="h-4 w-4 my-1 text-zinc-400" />
+                      <p class="text-blue-400 text-center text-sm"><%= @partner.location.name %></p>
+                    </div>
+                  <% end %>
+                </div>
+              </div>
+              <div>
+                <p class="pb-10 px-10 truncate text-xs leading-5 text-zinc-500 overflow-hidden overflow-ellipsis whitespace-normal">
+                  <%= Enum.map(String.split(@partner.benefits, "\n"), fn phrase -> %>
+                    <%= if String.length(phrase) < 50 do %>
+                      <%= phrase %><br />
+                    <% else %>
+                      <%= String.slice(phrase, 0..50) <> "..." %> <br />
+                    <% end %>
+                  <% end) %>
+                </p>
               </div>
             </div>
           </li>
