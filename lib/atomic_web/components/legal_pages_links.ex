68240d8a198727b88c3129a9ed374ea164706c89
RicoPleasure <esprazj.dev@gmail.com>
2024-11-25 23:39:21 +0000

    feat: add hover styles to links

--- lib/atomic_web/components/legal_pages_links.ex
+++ lib/atomic_web/components/legal_pages_links.ex
@@ -8,13 +8,13 @@ defmodule AtomicWeb.Components.LegalPagesLinks do
     ~H"""
     <div class="flex-colunm group mt-2 mb-4 px-8 flex w-full flex-wrap place-items-center justify-center gap-x-6 gap-y-1">
       <.link navigate={~p"/tos"} class="shrink-0 select-none">
-        <p class="text-xs font-semibold text-zinc-400">Terms of Service</p>
+        <p class="text-xs font-semibold text-zinc-400 hover:underline">Terms of Service</p>
       </.link>
       <.link navigate={~p"/privacy"} class="shrink-0 select-none">
-        <p class="text-xs font-semibold text-zinc-400">Privacy Policy</p>
+        <p class="text-xs font-semibold text-zinc-400 hover:underline">Privacy Policy</p>
       </.link>
       <.link navigate={~p"/cookies"} class="shrink-0 select-none">
-        <p class="text-xs font-semibold text-zinc-400">Cookie Policy</p>
+        <p class="text-xs font-semibold text-zinc-400 hover:underline">Cookie Policy</p>
       </.link>
       <span class="flex text-xs font-semibold text-zinc-400">&#169; 2024 CeSIUM</span>
     </div>
