5581af6e5b62e54300b50538702b4591c78fc34d
RicoPleasure <esprazj.dev@gmail.com>
2024-11-26 22:59:08 +0000

    fix: dinamic paths

--- lib/atomic_web/live/legal_terms_live/components/header.ex
+++ lib/atomic_web/live/legal_terms_live/components/header.ex
@@ -23,23 +23,23 @@ defmodule AtomicWeb.LegalTermsLive.Components.Header do
 
   def header(assigns) do
     ~H"""
-    <header class="fixed flex w-full max-w-[2000px] place-items-center justify-between bg-white pt-8 pr-8 pb-4 pl-4">
+    <header class="max-w-[2000px] fixed flex w-full place-items-center justify-between bg-white pt-8 pr-8 pb-4 pl-4">
       <div class="flex place-items-center gap-x-6 md:gap-x-12 lg:pl-12">
         <div class="flex place-items-center gap-x-4">
           <!-- Atomic Logo -->
           <.link navigate={~p"/"}>
             <img src={~p"/images/atomic.svg"} class="h-14 w-auto" />
           </.link>
-          </div>
+        </div>
         <div class="flex hidden place-items-center gap-x-2 text-sm font-semibold text-zinc-300 sm:block sm:gap-x-4 sm:space-x-2 md:space-x-4">
-        <%= for {type, title, path} <- link_pages(@page_name) do %>
-          <%= case type do %>
-            <% :current -> %>
-            <span class="text-lg text-zinc-400"> <%= title %> </span>
-            <% :link -> %>
-            <.link class="hover:text-zinc-400" navigate={~p"/#{path}"}><%= title %></.link>
+          <%= for {type, title, path} <- link_pages(@page_name) do %>
+            <%= case type do %>
+              <% :current -> %>
+                <span class="text-lg text-zinc-400"><%= title %></span>
+              <% :link -> %>
+                <.link class="hover:text-zinc-400" navigate={"/" <> path}><%= title %></.link>
+            <% end %>
           <% end %>
-        <% end %>
         </div>
       </div>
       <!-- Link to Home (hidden on mobile) -->
