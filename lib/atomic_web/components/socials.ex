fec1d1ef820ef9b1537f0357e5ac8e4344c00d99
Afonso Martins <afonsopm2005@gmail.com>
2024-11-26 16:01:12 +0000

    fix: capitalize

--- lib/atomic_web/components/socials.ex
+++ lib/atomic_web/components/socials.ex
@@ -13,9 +13,9 @@ defmodule AtomicWeb.Components.Socials do
       <%= for {social, icon, url_base, social_value} <- @socials_with_values do %>
         <%= if social_value do %>
           <div class="flex flex-row items-center gap-x-1">
-            <img src={"/images/" <> icon} class="h-5 w-5" alt={atom_to_string_capitalize(social)} />
-            <.link class="text-blue-500" target="_blank" href={url_base <> social_value}>
-              <%= atom_to_string_capitalize(social) %>
+            <img src={"/images/" <> icon} class="h-5 w-5" alt={Atom.to_string(social)} />
+            <.link class="capitalize text-blue-500" target="_blank" href={url_base <> social_value}>
+              <%= Atom.to_string(social) %>
             </.link>
           </div>
         <% end %>
