f1d8d3474da9526cd8e10c15d22a9aa8e4701748
AfonsoMartins26 <afonsopm2005@gmail.com>
2025-03-14 23:36:54 +0000

    feat: add activities as options

--- lib/atomic_web/live/organization_live/certificate_live/index.ex
+++ lib/atomic_web/live/organization_live/certificate_live/index.ex
@@ -6,7 +6,6 @@ defmodule AtomicWeb.OrganizationLive.CertificateLive.Index do
   import Atomic.Quantum.CertificateDelivery
   import AtomicWeb.Components.Forms
 
-
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
@@ -18,10 +17,9 @@ defmodule AtomicWeb.OrganizationLive.CertificateLive.Index do
     organization = Organizations.get_organization!(organization_id)
     changeset = Organizations.change_organization(organization)
 
-
     {:noreply,
      socket
-     |> assign(:page_title, gettext("Certeficate"))
+     |> assign(:page_title, gettext("Certificate"))
      |> assign(:current_page, :certificate)
      |> assign(:organization, organization)
      |> assign(:changeset, changeset)
@@ -30,17 +28,13 @@ defmodule AtomicWeb.OrganizationLive.CertificateLive.Index do
   end
 
   @impl true
-  def handle_event("generate",_params, socket) do
-    %{
-      enrollment: enrollment,
-      activity: activity,
-      organization: organization
-    } = socket.assigns
+  def handle_event("generate", _params, socket) do
+    with %{activity: activity, organization: organization} <- socket.assigns,
+         enrollment <- Activities.get_enrollment!(activity.id, user_id),
 
-    case generate_certificate(enrollment, activity, organization) do
-      {:ok, _pdf_path} ->
-        {:noreply, socket |> put_flash(:info, "Certificado gerado com sucesso!")}
-
+         {:ok, _pdf_path} <- generate_certificate(enrollment, activity, organization) do
+      {:noreply, socket |> put_flash(:info, "Certificado gerado com sucesso!")}
+    else
       {:error, reason} ->
         {:noreply, socket |> put_flash(:error, "Erro ao gerar certificado: #{reason}")}
     end
@@ -48,11 +42,14 @@ defmodule AtomicWeb.OrganizationLive.CertificateLive.Index do
 
   defp list_activities(organization_id) do
     case Activities.list_activities_by_organization_id(organization_id) do
-      {:ok, {activities, _meta}} ->
-        activities
-
-      {:error, _flop} ->
-        []
+      {:ok, {activities, _meta}} -> activities
+      {:error, _flop} -> []
     end
   end
+
+  defp options(activities) do
+    activities
+    |> Enum.map(&{&1.title, &1.id})
   end
+
+end
