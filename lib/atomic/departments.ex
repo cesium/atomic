b42c5c3645ba0245e23f56f4358ba0d8b33879c6
RicoPleasure <esprazj.dev@gmail.com>
2025-03-18 10:46:37 +0000

    format

--- lib/atomic/departments.ex
+++ lib/atomic/departments.ex
@@ -401,14 +401,17 @@ defmodule Atomic.Departments do
       [%User{}, ...]
 
   """
-def get_admin_collaborators(%Department{} = department) do
-  User
-  |> join(:inner, [u], c in assoc(u, :collaborators))
-  |> where([u, c], c.department_id == ^department.id and c.accepted == true)
-  |> join(:inner, [u, c], m in Membership, on: m.user_id == u.id)
-  |> where([u, c, m], m.organization_id == ^department.organization_id and m.role in [:admin, :owner])
-  |> Repo.all()
-end
+  def get_admin_collaborators(%Department{} = department) do
+    User
+    |> join(:inner, [u], c in assoc(u, :collaborators))
+    |> where([u, c], c.department_id == ^department.id and c.accepted == true)
+    |> join(:inner, [u, c], m in Membership, on: m.user_id == u.id)
+    |> where(
+      [u, c, m],
+      m.organization_id == ^department.organization_id and m.role in [:admin, :owner]
+    )
+    |> Repo.all()
+  end
 
   @doc """
   Request collaborator access and send email.
