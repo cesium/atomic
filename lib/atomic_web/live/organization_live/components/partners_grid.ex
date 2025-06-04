25a6e8bceb0694fbd9089f5eb2807aedf3e44036
GuilhermePSF <guilhermepsf23@gmail.com>
2025-06-04 16:31:12 +0100

    feat: avatar and logo in partner card

--- lib/atomic_web/live/organization_live/components/partners_grid.ex
+++ lib/atomic_web/live/organization_live/components/partners_grid.ex
@@ -6,6 +6,7 @@ defmodule AtomicWeb.OrganizationLive.Components.PartnersGrid do
 
   alias Atomic.Organizations.{Organization, Partner}
   alias Atomic.Repo
+  import AtomicWeb.Components.Avatar
   import Ecto.Query
 
   attr :organization, Organization,
@@ -33,10 +34,17 @@ defmodule AtomicWeb.OrganizationLive.Components.PartnersGrid do
 
   defp partner_card(assigns) do
     ~H"""
-    <div class="rounded-2xl border bg-white p-4 shadow transition hover:shadow-md">
-      <h3 class="text-lg font-semibold text-zinc-900"><%= @partner.name %></h3>
-      <p class="mt-1 text-sm text-zinc-600"><%= @partner.location.name %></p>
-      <p class="mt-2 line-clamp-3 text-sm text-zinc-500"><%= @partner.description %></p>
+    <div class="flex flex-col rounded-2xl border bg-white p-4 shadow transition hover:shadow-md">
+      <div class="flex flex-row">
+        <div class="mr-2">
+          <.avatar color={:light_zinc} name={@partner.name} src={Uploaders.PartnerImage.url({@partner.image, @partner}, :original)} type={:company} size={:sm} />
+        </div>
+        <div>
+          <h3 class="text-lg font-semibold text-zinc-900">{@partner.name}</h3>
+          <p class="text-sm text-zinc-600">  <.icon name="hero-map-pin" class="size-5 mb-1 text-zinc-400"/> {@partner.location.name}</p>
+        </div>
+      </div>
+      <p class="line-clamp-3 mt-2 text-sm text-zinc-500">{@partner.description}</p>
     </div>
     """
   end
